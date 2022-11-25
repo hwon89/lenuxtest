b710910f7d970eb7e88b1aea7689b3613b81e9f3
unknown <whm1482@naver.com>
2022-11-25 12:40:38 +0900

    third

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import logo from "./logo.svg";
+import axios from "axios";
+import { useState } from "react";
 import "./App.css";
-import PrintMyName from "./components/PrintMyName";
-
+import PrintMyname from "./components/PrintMyName";
 function App() {
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    const { data } = await axios.get("http://192.168.0.14:5000/user");
+    setUsers(data);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <PrintMyName name="곽건호"></PrintMyName>
-          <PrintMyName name="박광덕"></PrintMyName>
-        </p>
+        <button onClick={getUsers}>getUsers</button>
+        {users.map((el) => (
+          <PrintMyname name={el.name}></PrintMyname>
+        ))}
       </header>
     </div>
   );
 }
-
 export default App;
